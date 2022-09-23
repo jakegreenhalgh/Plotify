f25cd5fb045a34f0d0bea4694adc565e004d4d09
Jamie <jamiefogartykk@gmail.com>
2022-09-23 16:09:10 +0100

    fetched all 50 songs for ireland

--- src/mapcomponents/MapChart.js
+++ src/mapcomponents/MapChart.js
@@ -7,7 +7,26 @@ const geoUrl =
 
 function MapChart() {
 
-    const [clickedCountry, setClickedCountry] = useState("");
+  const [clickedCountry, setClickedCountry] = useState("");
+  const [playlist, setPlaylist] = useState([]);
+
+  const playlistId = '37i9dQZEVXbKM896FDX8L1'
+  const accessToken = 'BQBAtck9nbr0sDBTc6HJd1Wkrs3YOJfqiOkBdlIW6YcP_nwZdeJjEAKN0Qi-rkgn91NUJZSOCPAeyF-scMvCLE5aa1v2hEwzqceE7KVxOt8UFN230to7W9qfyc-iZZ-fbbT7cmNxq3Vqvt_MdfFcvYUZVMJ9OkB9lJMpWBoQWTrZaNg'
+
+    useEffect (() => {
+      fetch('https://api.spotify.com/v1/playlists/37i9dQZEVXbKM896FDX8L1', {
+            method: 'GET', headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            }
+        })
+            .then(res => res.json())
+            .then(top10 => setPlaylist(top10.tracks.items))
+    }, [])
+
+
+
     const handleClick = (geo) => {
         setClickedCountry(geo.properties.name);}
     
