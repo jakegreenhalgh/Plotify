bd0c6466fcb31f92a6a022db3ce64b8f8dfa86fd
Jamie <jamiefogartykk@gmail.com>
2022-09-26 14:31:57 +0100

    added more countries

---
+++ Client/src/CountryPlaylist.js
@@ -0,0 +1,824 @@
+
+const countryPlaylistId = 
+
+{"AD" : "Andorra",
+
+
+
+
+"AE" : "37i9dQZEVXbM4UZuIrvHvA",
+
+
+"AF" : "1m7Ov4UeE0XYD2P82Qnt8B",
+
+
+"AG" : "Antigua and Barbuda",
+
+
+"AI" : "Anguilla",
+
+
+"AL" : "Albania",
+
+
+"AM" : "Armenia",
+
+
+"AO" : "Angola",
+
+
+"AQ" : "Antarctica",
+
+
+"AR" : "37i9dQZEVXbMMy2roB9myp",
+
+
+"AS" : "American Samoa",
+
+
+"AT" : "Austria",
+
+
+"AU" : "37i9dQZEVXbJPcfkRz0wJ0",
+
+
+"AW" : "Aruba",
+
+
+"AX" : "Åland Islands",
+
+
+"AZ" : "Azerbaijan",
+
+
+//B
+
+
+"BA" : "Bosnia and Herzegovina",
+
+
+"BB" : "Barbados",
+
+
+"BD" : "Bangladesh",
+
+
+"BE" : "37i9dQZEVXbJNSeeHswcKB",
+
+
+"BF" : "Burkina Faso",
+
+
+"BG" : "Bulgaria",
+
+
+"BH" : "Bahrain",
+
+
+"BI" : "Burundi",
+
+
+"BJ" : "Benin",
+
+
+"BL" : "Saint Barthélemy",
+
+
+"BM" : "Bermuda",
+
+
+"BN" : "Brunei Darussalam",
+
+
+"BO" : "Bolivia : Plurinational State of",
+
+
+"BQ" : "Bonaire : Sint Eustatius and Saba",
+
+
+"BR" : "37i9dQZEVXbMXbN3EUUhlg",
+
+
+"BS" : "Bahamas",
+
+
+"BT" : "Bhutan",
+
+
+"BV" : "Bouvet Island",
+
+
+"BW" : "Botswana",
+
+
+"BY" : "Belarus",
+
+
+"BZ" : "Belize",
+
+
+//C
+
+
+"CA" : "37i9dQZEVXbKj23U1GF4IR",
+
+
+"CC" : "Cocos (Keeling) Islands",
+
+
+"CD" : "Congo : the Democratic Republic of",
+
+
+"CF" : "Central African Republic",
+
+
+"CG" : "Congo",
+
+
+"CH" : "37i9dQZEVXbJiyhoAPEfMK",
+
+
+"CI" : "Côte d'Ivoire",
+
+
+"CK" : "Cook Islands",
+
+
+"CL" : "Chile",
+
+
+"CM" : "Cameroon",
+
+
+"CN" : null,
+
+
+"CO" : "37i9dQZEVXbOa2lmxNORXQ",
+
+
+"CR" : "Costa Rica",
+
+
+"CU" : "Cuba",
+
+
+"CV" : "Cabo Verde",
+
+
+"CW" : "Curaçao",
+
+
+"CX" : "Christmas Island",
+
+
+"CY" : "Cyprus",
+
+
+"CZ" : "Czech Republic",
+
+
+//D
+
+
+"DE" : "37i9dQZEVXbJiZcmkrIHGU",
+
+
+"DJ" : "Djibouti",
+
+
+"DK" : "37i9dQZEVXbL3J0k32lWnN",
+
+
+"DM" : "Dominica",
+
+
+"DO" : "Dominican Republic",
+
+
+"DZ" : "Algeria",
+
+
+//E
+
+
+"EC" : "Ecuador",
+
+
+"EE" : "Estonia",
+
+
+"EG" : "Egypt",
+
+
+"EH" : "Western Sahara",
+
+
+"ER" : "Eritrea",
+
+
+"ES" : "37i9dQZEVXbNFJfN1Vw8d9",
+
+
+"ET" : "Ethiopia",
+
+
+//F
+
+
+"FI" : "37i9dQZEVXbMxcczTSoGwZ",
+
+
+"FJ" : "Fiji",
+
+
+"FK" : "Falkland Islands (Malvinas)",
+
+
+"FM" : "Micronesia : Federated States of",
+
+
+"FO" : "Faroe Islands",
+
+
+"FR" : "37i9dQZEVXbIPWwFssbupI",
+
+
+//G
+
+
+"GA" : "Gabon",
+
+
+"GB" : "37i9dQZEVXbLnolsZ8PSNw",
+
+
+"GD" : "Grenada",
+
+
+"GE" : "Georgia",
+
+
+"GF" : "French Guiana",
+
+
+"GG" : "Guernsey",
+
+
+"GH" : "Ghana",
+
+
+"GI" : "Gibraltar",
+
+
+"GL" : "Greenland",
+
+
+"GM" : "Gambia",
+
+
+"GN" : "Guinea",
+
+
+"GP" : "Guadeloupe",
+
+
+"GQ" : "Equatorial Guinea",
+
+
+"GR" : "37i9dQZEVXbJqdarpmTJDL",
+
+
+"GS" : "South Georgia and the South Sandwich Islands",
+
+
+"GT" : "Guatemala",
+
+
+"GU" : "Guam",
+
+
+"GW" : "Guinea-Bissau",
+
+
+"GY" : "Guyana",
+
+
+//H
+
+
+"HK" : "37i9dQZEVXbLwpL8TjsxOG",
+
+
+"HM" : "Heard Island and McDonalds Islands",
+
+
+"HN" : "Honduras",
+
+
+"HR" : "Croatia",
+
+
+"HT" : "Haiti",
+
+
+"HU" : "Hungary",
+
+
+//I
+
+
+"ID" : "Indonesia",
+
+
+"IE" : "37i9dQZEVXbKM896FDX8L1",
+
+
+"IL" : "Israel",
+
+
+"IM" : "Isle of Man",
+
+
+"IN" : "37i9dQZEVXbLZ52XmnySJg",
+
+
+"IO" : "British Indian Ocean Territory",
+
+
+"IQ" : "Iraq",
+
+
+"IR" : "Iran : Islamic Republic of",
+
+
+"IS" : "Iceland",
+
+
+"IT" : "37i9dQZEVXbIQnj7RRhdSX",
+
+
+//J
+
+
+"JE" : "Jersey",
+
+
+"JM" : "Jamaica",
+
+
+"JO" : "Jordan",
+
+
+"JP" : "37i9dQZEVXbKXQ4mDTEBXq",
+
+
+//K
+
+
+"KE" : "Kenya",
+
+
+"KG" : "Kyrgyzstan",
+
+
+"KH" : "Cambodia",
+
+
+"KI" : "Kiribati",
+
+
+"KM" : "Comoros",
+
+
+"KN" : "Saint Kitts and Nevis",
+
+
+"KP" : "Korea : Democratic People's Republic of",
+
+
+"KR" : "37i9dQZEVXbNxXF4SkHj9F",
+
+
+"KW" : "Kuwait",
+
+
+"KY" : "Cayman Islands",
+
+
+"KZ" : "Kazakhstan",
+
+
+//L
+
+
+"LA" : "Lao People's Democratic Republic",
+
+
+"LB" : "Lebanon",
+
+
+"LC" : "Saint Lucia",
+
+
+"LI" : "Liechtenstein",
+
+
+"LK" : "Sri Lanka",
+
+
+"LR" : "Liberia",
+
+
+"LS" : "Lesotho",
+
+
+"LT" : "37i9dQZEVXbMx56Rdq5lwc",
+
+
+"LU" : "Luxembourg",
+
+
+"LV" : "Latvia",
+
+
+//M
+
+
+"MA" : "37i9dQZEVXbJU9eQpX8gPT",
+
+
+"MC" : "Monaco",
+
+
+"MD" : "Moldova : Republic of",
+
+
+"ME" : "Montenegro",
+
+
+"MF" : "Saint Martin (French part)",
+
+
+"MG" : "Madagascar",
+
+
+"MH" : "Marshall Islands",
+
+
+"MK" : "Macedonia : the former Yugoslav Republic of",
+
+
+"ML" : "Mali",
+
+
+"MM" : "Myanmar",
+
+
+"MN" : "Mongolia",
+
+
+"MO" : "Macao",
+
+
+"MP" : "Northern Mariana Islands",
+
+
+"MQ" : "Martinique",
+
+
+"MR" : "Mauritania",
+
+
+"MS" : "Montserrat",
+
+
+"MT" : "Malta",
+
+
+"MU" : "Mauritius",
+
+
+"MV" : "Maldives",
+
+
+"MW" : "Malawi",
+
+
+"MX" : "37i9dQZEVXbO3qyFxbkOE1",
+
+
+"MY" : "Malaysia",
+
+
+"MZ" : "Mozambique",
+
+
+//N
+
+
+"NA" : "Namibia",
+
+
+"NC" : "New Caledonia",
+
+
+"NE" : "Niger",
+
+
+"NF" : "Norfolk Island",
+
+
+"NG" : "Nigeria",
+
+
+"NI" : "Nicaragua",
+
+
+"NL" : "Netherlands",
+
+
+"NO" : "37i9dQZEVXbJvfa0Yxg7E7",
+
+
+"NP" : "Nepal",
+
+
+"NR" : "Nauru",
+
+
+"NU" : "Niue",
+
+
+"NZ" : "New Zealand",
+
+
+//O
+
+
+"OM" : "Oman",
+
+
+//P
+
+
+"PA" : "Panama",
+
+
+"PE" : "37i9dQZEVXbJfdy5b0KP7W",
+
+
+"PF" : "French Polynesia",
+
+
+"PG" : "Papua New Guinea",
+
+
+"PH" : "37i9dQZEVXbNBz9cRCSFkY",
+
+
+"PK" : "Pakistan",
+
+
+"PL" : "37i9dQZEVXbN6itCcaL3Tt",
+
+
+"PM" : "Saint Pierre and Miquelon",
+
+
+"PN" : "Pitcairn",
+
+
+"PR" : "Puerto Rico",
+
+
+"PS" : "Palestine : State of",
+
+
+"PT" : "37i9dQZEVXbKyJS56d1pgi",
+
+
+"PW" : "Palau",
+
+
+"PY" : "Paraguay",
+
+
+//Q
+
+
+"QA" : "Qatar",
+
+
+//R
+
+
+"RE" : "Réunion",
+
+
+"RO" : "Romania",
+
+
+"RS" : "Serbia",
+
+
+"RU" : null,
+
+
+"RW" : "Rwanda",
+
+
+//S
+
+
+"SA" : "37i9dQZEVXbLrQBcXqUtaC",
+
+
+"SB" : "Solomon Islands",
+
+
+"SC" : "Seychelles",
+
+
+"SD" : "Sudan",
+
+
+"SE" : "37i9dQZEVXbLoATJ81JYXz",
+
+
+"SG" : "Singapore",
+
+
+"SH" : "Saint Helena : Ascension and Tristan da Cunha",
+
+
+"SI" : "Slovenia",
+
+
+"SJ" : "Svalbard and Jan Mayen",
+
+
+"SK" : "Slovakia",
+
+
+"SL" : "Sierra Leone",
+
+
+"SM" : "San Marino",
+
+
+"SN" : "Senegal",
+
+
+"SO" : "Somalia",
+
+
+"SR" : "Suriname",
+
+
+"SS" : "South Sudan",
+
+
+"ST" : "Sao Tome and Principe",
+
+
+"SV" : "El Salvador",
+
+
+"SX" : "Sint Maarten (Dutch part)",
+
+
+"SY" : "Syrian Arab Republic",
+
+
+"SZ" : "Swaziland",
+
+
+//T
+
+
+"TC" : "Turks and Caicos Islands",
+
+
+"TD" : "Chad",
+
+
+"TF" : "French Southern Territories",
+
+
+"TG" : "Togo",
+
+
+"TH" : "Thailand",
+
+
+"TJ" : "Tajikistan",
+
+
+"TK" : "Tokelau",
+
+
+"TL" : "Timor-Leste",
+
+
+"TM" : "Turkmenistan",
+
+
+"TN" : "Tunisia",
+
+
+"TO" : "Tonga",
+
+
+"TR" : "37i9dQZEVXbIVYVBNw9D5K",
+
+
+"TT" : "Tuvalu",
+
+
+"TW" : "37i9dQZEVXbMnZEatlMSiu",
+
+
+"TZ" : "Tanzania : United Republic of",
+
+
+//U
+
+
+"UA" : "Ukraine",
+
+
+"UG" : "Uganda",
+
+
+"UM" : "United States Minor Outlying Islands",
+
+
+"US" : "37i9dQZEVXbLRQDuF5jeBp",
+
+
+"UY" : "Uruguay",
+
+
+"UZ" : "Uzbekistan",
+
+
+//V
+
+
+"VA" : "Holy See",
+
+
+"VC" : "Saint Vincent and the Grenadines",
+
+
+"VE" : "Venezuela : Bolivarian Republic of",
+
+
+"VG" : "Virgin Islands : British",
+
+
+"VI" : "Virgin Islands : U.S.",
+
+
+"VN" : "Viet Nam",
+
+
+"VU" : "Vanuatu",
+
+
+//W
+
+
+"WF" : "Wallis and Futuna",
+
+
+"WS" : "Samoa",
+
+
+//Y
+
+
+"YE" : "Yemen",
+
+
+"YT" : "Mayotte",
+
+
+//Z
+
+
+"ZA" : "South Africa",
+
+
+"ZM" : "Zambia",
+
+
+"ZW" : "Zimbabwe"
+
+
+}
+
+// console.log(countryPlaylistId.GB);
+// console.log(countryPlaylistId["GB"]);
+
+export default countryPlaylistId;
