9acda8fcd7025c84c6fbbf9474afe153f2009b19
Jacob <jacob_greenhalgh@hotmail.com>
2022-10-05 13:13:38 +0100

    more styling

--- Client/src/Login.js
+++ Client/src/Login.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from './images/logothing.png';
+import './App.css';
 
 function Login() {
     return (
         <div className="App">
             <header className="App-header">
+                <img src={logo} alt='logo' className='login-logo'/> 
                 <h2>Welcome to Plotify</h2>
                 <h3>Where you can see what music is trending by just clicking on a plot of land</h3>
                 <a className="btn-spotify" href="/auth/login" >
