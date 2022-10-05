528809875982a23c24f9b91784f732fdedb76e1f
Jamie <jamiefogartykk@gmail.com>
2022-10-05 10:09:56 +0100

    quiz styling

--- Client/src/quizComponents/QuizItem.js
+++ Client/src/quizComponents/QuizItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const QuizItem = ({songName, songID, songArtists, checkQuestion, answeredQuestions, preview}) => {
+const QuizItem = ({songName, songID, songArtists, checkQuestion, answeredQuestions, preview, songImage}) => {
 
 let audio = new Audio(preview)
 
@@ -19,11 +19,50 @@ const pauseTrack = () => {
 
 const getButtonStatus = (answeredQuestions, songID) => {
     if (!answeredQuestions.hasOwnProperty(songID)){
-        return <QuestionUnanswered onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionUnanswered>
+        return <QuestionUnanswered>
+                <Wrapper onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess}>
+        <SongImage src={songImage}/>
+        <Details>
+            <UserName>
+                {songName}
+            </UserName>
+            <Info>
+            {songName} by {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}
+            </Info>
+        </Details>
+    </Wrapper>
+        </QuestionUnanswered>
+        // return <QuestionUnanswered onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionUnanswered>
     } else if (answeredQuestions[songID]) {
-        return <QuestionCorrect onMouseEnter={playTrack} onMouseLeave={pauseTrack} disabled={answeredQuestions.hasOwnProperty(songID)}  onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionCorrect>
+        return <QuestionCorrect>
+            <Wrapper onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess} disabled={true}>
+        <SongImage src={songImage}/>
+        <Details>
+            <Comment>
+                {songName}
+            </Comment>
+            <Info>
+            {songName} by {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}
+            </Info>
+        </Details>
+    </Wrapper>
+        </QuestionCorrect>
+        // return <QuestionCorrect onMouseEnter={playTrack} onMouseLeave={pauseTrack} disabled={answeredQuestions.hasOwnProperty(songID)}  onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionCorrect>
     } else {
-        return <QuestionIncorrect onMouseEnter={playTrack} disabled={answeredQuestions.hasOwnProperty(songID)}  onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionIncorrect>
+        return <QuestionIncorrect>
+            <Wrapper onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess} disabled={true}>
+        <SongImage src={songImage}/>
+        <Details>
+            <UserName>
+                {songName}
+            </UserName>
+            <Info>
+            {songName} by {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}
+            </Info>
+        </Details>
+    </Wrapper>
+        </QuestionIncorrect>
+        // return <QuestionIncorrect onMouseEnter={playTrack} disabled={answeredQuestions.hasOwnProperty(songID)}  onClick={handleGuess}>{songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}</QuestionIncorrect>
     }
 }
 const buttonStatus = getButtonStatus(answeredQuestions, songID)
@@ -33,10 +72,6 @@ const buttonStatus = getButtonStatus(answeredQuestions, songID)
     <>
     {buttonStatus}
     </>
-    // <button disabled={answeredQuestions.hasOwnProperty(songID)}  onClick={handleGuess}>
-    //     {songID}
-    //     {songName} | {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}
-    // </button>
   )
 }
 
@@ -45,14 +80,84 @@ export default QuizItem
 const QuestionUnanswered = styled.button`
     margin-right: 4px;
     color: gray;
+    background-color: gray;
+    
 `
 
 const QuestionCorrect = styled.button`
     margin-right: 4px;
     color: green;
+    background-color: green;
 `
 
 const QuestionIncorrect = styled.button`
     margin-right: 4px;
     color: red;
+    background-color: red;
 `
+// return (
+//     <Wrapper onMouseEnter={playTrack} onMouseLeave={pauseTrack} onClick={handleGuess}>
+//         <ProfileImage src={songImage}/>
+//         <Details>
+//             <UserName>
+//                 {songName}
+//             </UserName>
+//             <Info>
+//             {songArtists[0].name}{songArtists[1]?", "+songArtists[1].name:null}
+//             </Info>
+//         </Details>
+//     </Wrapper>
+// )
+
+const Wrapper = styled.li`
+    display: flex;
+    margin: 10px auto;
+    width: 500px;
+    /* background-color: #efebebeb; */
+    padding: 10px;
+    border-radius: 5px;
+
+`
+
+const SongImage = styled.img.attrs(props => ({ src: props.src }))`
+    width: 60px;
+    height: 60px;
+    opacity: 0.8;
+    border-style: solid;
+`;
+
+const Comment = styled.blockquote`
+    margin: 10px 0;
+    font-size: 1.1rem;
+`;
+
+const Info = styled.span`
+    color: #5252f9; 
+    a {
+        text-decoration: bold;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+    font-size: 0.8rem;
+`;
+
+const Details = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 10px;
+`;
+
+const UserName = styled.div`
+    color: #5252f9; 
+    a {
+        text-decoration: none;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`;
--- Client/src/quizComponents/QuizQuestions.js
+++ Client/src/quizComponents/QuizQuestions.js
@@ -5,7 +5,7 @@ const QuizQuestions = ({questions, checkQuestion, answeredQuestions}) => {
     const questionsList = questions.map((question) => {
         return <QuizItem answeredQuestions={answeredQuestions} songName={question.name} 
         preview={question.preview_url} 
-        key={question.id} songID={question.id} image={question.album.images[2].url}checkQuestion={checkQuestion} songArtists={question.artists}/>
+        key={question.id} songID={question.id} songImage={question.album.images[2].url}checkQuestion={checkQuestion} songArtists={question.artists}/>
     })
   return (
     <>
